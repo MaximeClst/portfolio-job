f1e5e5c605af1cdb40b8f0be423c2ddcf2e14f41
Maxime <maxime.celeste1221@gmail.com>
2024-06-04 15:36:36 +0400

    modif Hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,24 +1,39 @@
-/* eslint-disable @next/next/no-img-element */
+/*  eslint-disable @next/next/no-img-element */
+import { cn } from "@/lib/utils";
+import { ComponentPropsWithoutRef } from "react";
 import { Section } from "./Section";
+
+const Code = ({ className, ...props }: ComponentPropsWithoutRef<"span">) => {
+  return (
+    <span
+      className={cn(
+        "bg-accent/30 font-mono border border-accent p-1 text-primary rounded-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+};
 
 export const Hero = () => {
   return (
-    <Section className="flex max-lg:flex-col items-start">
-      <div className="flex-[2]">
-        <h2 className="font-caption text-5xl">Maxime CELESTE</h2>
+    <Section className="flex max-lg:flex-col items-start gap-4">
+      <div className="flex-[2] flex flex-col gap-2">
+        <h2 className="font-caption text-5xl text-primary">Maxime CELESTE</h2>
         <h3 className="font-caption text-3xl">FrontEnd Developer</h3>
         <p>
           Développeur Front End Junior passionné par la création
-          d&#39;interfaces utilisateurs modernes et performantes. Expertise en
-          React, Tailwind CSS, Next.js et TypeScript. Motivé par les défis
-          techniques et l&#39;apprentissage continu, je m&#39;engage à apporter
-          des solutions innovantes et efficaces.
+          d&#39;interfaces utilisateurs modernes et performantes. Expertise en{" "}
+          <Code>React</Code>, <Code>Tailwind CSS</Code>, <Code>Next.js</Code> et{" "}
+          <Code>TypeScript</Code>. Motivé par les défis techniques et
+          l&#39;apprentissage continu, je m&#39;engage à apporter des solutions
+          innovantes et efficaces.
         </p>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 max-md:m-auto ml-auto">
         <img
           src="https://avatars.githubusercontent.com/u/122441440?v=4"
-          className="w-full h-auto max-w-xs rounded-full"
+          className="w-full h-auto max-w-xs max-md:w-56 rounded-full"
           alt="maxime' picture"
         />
       </div>
