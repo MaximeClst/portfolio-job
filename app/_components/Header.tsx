071155b0635a48168b1cf4b4f32c80cbe9a7d9de
Maxime <maxime.celeste1221@gmail.com>
2024-06-04 14:23:26 +0400

    add icons header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,19 +1,36 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { Section } from "./Section";
 import { GithubIcon } from "./icons/GithubIcon";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
+import { TwitterIcon } from "./icons/TwitterIcon";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 py-4">
       <Section className="flex items-baseline">
-        <h1 className="text-4xl font-bold text-primary-foreground">
-          maximeclst.com
-        </h1>
+        <h1 className="text-lg font-bold text-primary">maximeclst.com</h1>
         <div className="flex-1" />
-        <ul>
-          <Button className="size-6">
-            <GithubIcon size={16} className="text-foreground" />
-          </Button>
+        <ul className="flex items-center gap-2">
+          <Link
+            href="https://github.com/MaximeClst"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <GithubIcon size={12} className="text-foreground" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/celeste-maxime-581352235/"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <LinkedinIcon size={12} className="text-foreground" />
+          </Link>
+          <Link
+            href="https://x.com/maxime_clst"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <TwitterIcon size={12} className="text-foreground" />
+          </Link>
         </ul>
       </Section>
     </header>
